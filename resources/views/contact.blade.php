74d8e4567780f6f2504f9844342e508535e49e3b
Killiam <killiam1401@gmail.com>
2024-11-10 01:56:39 -0500

    Descripción: prueba de estilo en vista contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,15 +1,24 @@
 <!-- resources/views/contact.blade.php -->
 
 <x-app-layout>
-    <div class="container mx-auto mt-10">
-        <h1 class="text-center text-3xl font-bold mb-6">Contáctanos</h1>
-        <p class="text-center">Puedes contactarnos por los siguientes medios:</p>
-        <ul class="mt-4 text-center">
-            <li><strong>Correo electrónico:</strong> alertasmart@gmail.com</li>
-            <li><strong>Líneas telefónicas:</strong></li>
-            <li>Celular: xxx-xxx-xxxx</li>
-            <li>Teléfono fijo: 601-xxxxxxx</li>
-            <li>WhatsApp: xxx-xxx-xxxx</li>
-        </ul>
+    <div class="container mx-auto mt-10 px-4 sm:px-6 lg:px-8">
+        <div class="bg-gradient-to-r from-blue-600 to-purple-600 text-white p-8 rounded-lg shadow-lg">
+            <h1 class="text-center text-4xl font-extrabold mb-6">Contáctanos</h1>
+            <p class="text-center text-lg mb-6">Puedes contactarnos por los siguientes medios:</p>
+            <ul class="space-y-4 text-center">
+                <li class="flex justify-center items-center">
+                    <span class="font-semibold">Correo electrónico:</span>
+                    <span class="ml-2 text-white/80">alertasmart@gmail.com</span>
+                </li>
+                <li class="flex flex-col justify-center items-center">
+                    <span class="font-semibold">Líneas telefónicas:</span>
+                    <ul class="space-y-2 mt-2">
+                        <li class="text-white/80">Celular: xxx-xxx-xxxx</li>
+                        <li class="text-white/80">Teléfono fijo: 601-xxxxxxx</li>
+                        <li class="text-white/80">WhatsApp: xxx-xxx-xxxx</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </x-app-layout>
